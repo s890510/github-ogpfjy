bc5d0f871103a8ea47c3f92f99e0b94f6c482efe
Evan Lin <evanwimoon@gmail.com>
2020-06-04 00:54:06 +0800

    3 files modified

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -3,212 +3,200 @@ import {MatTableDataSource} from '@angular/material/table';
 import {FormControl} from '@angular/forms';
 import {isNumeric} from 'rxjs/internal-compatibility';
 import {crc16ccitt} from 'crc';
-import {SHA1} from 'crypto-js'
+import {SHA1} from 'crypto-js';
 
 export interface QRCodeElement {
-  id: number;
-  label: number;
-  data: string;
-  description: string;
+    id: number;
+    label: number;
+    data: string;
+    description: string;
 }
 
 @Component({
-  selector: 'app-qrcode',
-  templateUrl: './qrcode.component.html',
-  styleUrls: ['./qrcode.component.css']
+    selector: 'app-qrcode',
+    templateUrl: './qrcode.component.html',
+    styleUrls: ['./qrcode.component.css']
 })
 
 export class QRCodeComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'label', 'data', 'description', 'delete'];
-  elementData: QRCodeElement[] = [];
-  dataSource = new MatTableDataSource(this.elementData);
+    displayedColumns: string[] = ['id', 'label', 'data', 'description', 'delete'];
+    elementData: QRCodeElement[] = [];
+    dataSource = new MatTableDataSource(this.elementData);
 
-  dataTag: string[] = [];
-  dataTag62: QRCodeElement[] = [];
-  dataTag64: QRCodeElement[] = [];
+    dataTag: string[] = [];
+    dataTag62: QRCodeElement[] = [];
+    dataTag64: QRCodeElement[] = [];
 
-  formDataId: FormControl;
-  formDataLabel: FormControl;
-  formDataData: FormControl;
+    formDataId: FormControl;
+    formDataLabel: FormControl;
+    formDataData: FormControl;
 
-  outputData = '';
+    outputData = '';
 
-  tag: number[] = [];
+    tag: number[] = [];
 
-  constructor() {
-    this.resetFormData();
-  }
-
-  ngOnInit(): void {
-  }
-
-  private resetFormData() {
-    this.formDataId = new FormControl('');
-    this.formDataLabel = new FormControl('');
-    this.formDataData = new FormControl('');
-  }
-
-  // checkValue()
-
-  addTag() {
-    const data: QRCodeElement = {} as any;
-
-    if (isNumeric(this.formDataId.value) && this.formDataId.value >= 0 && this.formDataId.value < 100) {
-      data.id = Number(this.formDataId.value);
-    }
-
-    if (isNumeric(this.formDataLabel.value) &&
-        this.formDataId.value >= 0 && this.formDataId.value < 100 && (data.id === 62 || data.id === 64)) {
-      data.label = Number(this.formDataLabel.value);
-    }
-
-    if (this.formDataData.value.length > 0) {
-      data.data = this.formDataData.value;
-    }
-
-    let checkIndex = SHA1(String(data.id) + String(data.label)).toString();
-    if (this.dataTag.indexOf(checkIndex) >= 0 || data.id == 63) {
-      this.resetFormData();
-      return;
-    }
-    
-    if (data.id === 62 || data.id === 64) {
-      if (isNaN(data.label)) {
+    constructor() {
         this.resetFormData();
-        return;
-      }
-    } 
-
-    switch (data.id) {
-      case 62:
-        this.dataTag62.push(data);
-        break;
-      case 64:
-        this.dataTag64.push(data);
-        break;
-      default:
-        this.dataTag.push(checkIndex);
-        break;
     }
 
-    this.elementData.push(data);
-    this.dataSource = new MatTableDataSource(this.elementData);
+    ngOnInit(): void {
+    }
 
-    console.log(this.dataTag);
+    addTag() {
+        const data: QRCodeElement = {} as any;
 
-    this.dataSource.data.sort((a: QRCodeElement, b: QRCodeElement) => {
-      if (a.id < b.id) {
-        return -1;
-      } else if (a.id > b.id) {
-        return 1;
-      } else {
-        if (!isNaN(a.label) && !isNaN(b.label)) {
-          if (a.label < b.label) {
-            return -1;
-          } else if (a.label > b.label) {
-            return 1;
-          }
+        if (isNumeric(this.formDataId.value) && this.formDataId.value >= 0 && this.formDataId.value < 100) {
+            data.id = Number(this.formDataId.value);
         }
-        return 0;
-      }
-    });
 
-    this.resetFormData();
-    this.updateCodeData();
-  }
+        if (isNumeric(this.formDataLabel.value) &&
+            this.formDataId.value >= 0 && this.formDataId.value < 100 && (data.id === 62 || data.id === 64)) {
+            data.label = Number(this.formDataLabel.value);
+        }
 
-  calcSpecialTag(tagId: number): string {
-    let str = '';
-    let data: QRCodeElement[];
+        if (this.formDataData.value.length > 0) {
+            data.data = this.formDataData.value;
+        }
 
-    console.log('calcSpecialTag');
-    console.log(tagId);
-    switch(tagId) {
-      case 62:
-        data = this.dataTag62;
-        break;
-      case 64:
-        data = this.dataTag64;
-        break;
-      default:
-        return null;
-    }
-    
-    console.log('AAABB');
-    console.log(data);
-    console.log(data.length);
+        const checkIndex = SHA1(String(data.id) + String(data.label)).toString();
+        if (this.dataTag.indexOf(checkIndex) >= 0 || data.id === 63) {
+            this.resetFormData();
+            return;
+        }
 
-    for (const d of data) {
-      console.log(d);
-      str += this.prefixInteger(d.label, 2) + this.prefixInteger(d.data.length, 2) + d.data
-      console.log(str);
-    }
+        if (data.id === 62 || data.id === 64) {
+            if (isNaN(data.label)) {
+                this.resetFormData();
+                return;
+            }
+        }
 
-    str = this.prefixInteger(tagId, 2) + this.prefixInteger(str.length, 2) + str;
+        switch (data.id) {
+            case 62:
+                this.dataTag62.push(data);
+                break;
+            case 64:
+                this.dataTag64.push(data);
+                break;
+            default:
+                this.dataTag.push(checkIndex);
+                break;
+        }
 
-    return str;
-  }
+        this.elementData.push(data);
+        this.dataSource = new MatTableDataSource(this.elementData);
 
-  updateCodeData() {
-    let str = '';
+        console.log(this.dataTag);
 
-    console.log("updateCodeData");
-    console.log(this.dataTag62);
-    console.log(this.dataTag64);
+        this.dataSource.data.sort((a: QRCodeElement, b: QRCodeElement) => {
+            if (a.id < b.id) {
+                return -1;
+            } else if (a.id > b.id) {
+                return 1;
+            } else {
+                if (!isNaN(a.label) && !isNaN(b.label)) {
+                    if (a.label < b.label) {
+                        return -1;
+                    } else if (a.label > b.label) {
+                        return 1;
+                    }
+                }
+                return 0;
+            }
+        });
 
-    let alreadyLoad = [];
-
-    for (const data of this.elementData) {
-      switch (data.id) {
-        case 62:
-        case 64:
-          if (isNaN(alreadyLoad[data.id])) {
-            str += this.calcSpecialTag(data.id);
-            alreadyLoad[data.id] = true;
-          }
-          break;
-        default:
-          str += this.prefixInteger(data.id, 2) + this.prefixInteger(data.data.length, 2) + data.data;
-      }
+        this.resetFormData();
+        this.updateCodeData();
     }
 
-    str += '6304';
-    str += crc16ccitt(str).toString(16).toUpperCase();
+    // checkValue()
 
-    this.outputData = str;
-  }
+    calcSpecialTag(tagId: number): string {
+        let str = '';
+        let data: QRCodeElement[];
 
-  setNumberPrefix(num, length): string {
-    return isNaN(num) ? '--' : this.prefixInteger(num, length);
-  }
+        switch (tagId) {
+            case 62:
+                data = this.dataTag62;
+                break;
+            case 64:
+                data = this.dataTag64;
+                break;
+            default:
+                return null;
+        }
 
-  prefixInteger(num, length) {
-    return (Array(length).join('0') + num).slice(-length);
-  }
+        for (const d of data) {
+            str += this.prefixInteger(d.label, 2) + this.prefixInteger(d.data.length, 2) + d.data;
+        }
 
-  updateData(value: HTMLInputElement) {
+        str = this.prefixInteger(tagId, 2) + this.prefixInteger(str.length, 2) + str;
 
-    const elementResult = this.elementData.find(dataElement =>
-      SHA1(String(dataElement.id) + String(dataElement.label)).toString() == (value.id));
-
-    elementResult.data = value.value;
-
-    this.dataSource = new MatTableDataSource(this.elementData);
-    this.updateCodeData();
-  }
-
-  getIndexId(id: number, label: number): string {
-    return SHA1(String(id) + String(label)).toString();
-  }
-
-  delTag(element: QRCodeElement) {
-    const index = this.elementData.indexOf(element);
-    
-    if (index >= 0) {
-      this.elementData.splice(index, 1);
+        return str;
     }
 
-    this.dataSource = new MatTableDataSource(this.elementData);
-    this.updateCodeData();
-  }
+    updateCodeData() {
+        let str = '';
+
+        const alreadyLoad = [];
+
+        for (const data of this.elementData) {
+            switch (data.id) {
+                case 62:
+                case 64:
+                    if (isNaN(alreadyLoad[data.id])) {
+                        str += this.calcSpecialTag(data.id);
+                        alreadyLoad[data.id] = true;
+                    }
+                    break;
+                default:
+                    str += this.prefixInteger(data.id, 2) + this.prefixInteger(data.data.length, 2) + data.data;
+            }
+        }
+
+        str += '6304';
+        str += crc16ccitt(str).toString(16).toUpperCase();
+
+        this.outputData = str;
+    }
+
+    setNumberPrefix(num, length): string {
+        return isNaN(num) ? '--' : this.prefixInteger(num, length);
+    }
+
+    prefixInteger(num, length) {
+        return (Array(length).join('0') + num).slice(-length);
+    }
+
+    updateData(value: HTMLInputElement) {
+
+        const elementResult = this.elementData.find(dataElement =>
+            SHA1(String(dataElement.id) + String(dataElement.label)).toString() === value.id);
+
+        elementResult.data = value.value;
+
+        this.dataSource = new MatTableDataSource(this.elementData);
+        this.updateCodeData();
+    }
+
+    getIndexId(id: number, label: number): string {
+        return SHA1(String(id) + String(label)).toString();
+    }
+
+    delTag(element: QRCodeElement) {
+        const index = this.elementData.indexOf(element);
+
+        if (index >= 0) {
+            this.elementData.splice(index, 1);
+        }
+
+        this.dataSource = new MatTableDataSource(this.elementData);
+        this.updateCodeData();
+    }
+
+    private resetFormData() {
+        this.formDataId = new FormControl('');
+        this.formDataLabel = new FormControl('');
+        this.formDataData = new FormControl('');
+    }
 }
